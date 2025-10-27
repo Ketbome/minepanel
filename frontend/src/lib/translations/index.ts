474f955db8da57dbef5588fd8e0ac4fc4f264c7c
stefanpost <stefanpost03@gmail.com>
2025-10-27 17:21:35 +0100

    ADD: dutch to the translation options

--- frontend/src/lib/translations/index.ts
+++ frontend/src/lib/translations/index.ts
@@ -1,9 +1,11 @@
 import { es } from './es';
 import { en } from './en';
+import { nl } from './nl';
 
 export const translations = {
   es,
   en,
+  nl
 };
 
 export type Language = keyof typeof translations;
