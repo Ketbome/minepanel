a3dc7ee291ed4b1a00f60167416c1afd87e01da4
Ketbome <Pablo.moraga.san@gmail.com>
2025-12-04 14:26:25 -0300

    fix: update doc links to new pages (networking, administration)

--- frontend/src/lib/providers/constants.ts
+++ frontend/src/lib/providers/constants.ts
@@ -1,7 +1,7 @@
 export const LINK = "https://minepanel.ketbome.lat";
 export const LINK_GITHUB = "https://github.com/Ketbome/minepanel";
 export const LINK_DOCUMENTATION = `${LINK}/configuration.html`;
-export const LINK_LEARN_HOW_LAN = `${LINK}/configuration.html#how-to-get-your-lan-ip`;
-export const LINK_FORGOT_PASSWORD = `${LINK}/configuration.html#forgot-password`;
-export const LINK_FILEBROWSER_PASSWORD = `${LINK}/configuration.html#filebrowser-password`;
+export const LINK_LEARN_HOW_LAN = `${LINK}/networking.html#how-to-get-your-lan-ip`;
+export const LINK_FORGOT_PASSWORD = `${LINK}/administration.html#forgot-your-password`;
+export const LINK_FILEBROWSER_PASSWORD = `${LINK}/administration.html#filebrowser-password`;
 export const LINK_MODS_PLUGINS = `${LINK}/mods-plugins.html`;
